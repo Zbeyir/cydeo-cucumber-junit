6917400041b2ce533aaed9dc9214d0173b58644e
zübeyir <zeynelzatari@gmail.com>
2022-06-15 16:03:24 +0200

    Day 16

--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinition.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinition.java
@@ -14,10 +14,35 @@ public class GoogleStepDefinition {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
+    @When("user types {string} and clicks enter")
+    public void user_types_and_clicks_enter(String string) {
+
+        googleSearchPage.searchBox.sendKeys(string + Keys.ENTER);
+
+    }
+
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String string) {
+
+        String expectedTitle = string + " - Google'da Ara";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!", expectedTitle, actualTitle);
+
+
+    }
+
+
+
+
+
     @When("user types apple and clicks enter")
     public void user_types_apple_and_clicks_enter() {
+
         googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
     }
+
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
 
@@ -31,16 +56,16 @@ public class GoogleStepDefinition {
     }
 
 
-
     @When("user is on Google search page")
     public void user_is_on_google_search_page() {
 
         Driver.getDriver().get("https://www.google.com");
-       // BrowserUtils.sleep(2);
+        // BrowserUtils.sleep(2);
         WebElement acceptCookiesButton = Driver.getDriver().findElement(By.xpath("(//div[@class='QS5gu sy4vM'])[2]"));
         acceptCookiesButton.click();
 
     }
+
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
 
@@ -50,7 +75,6 @@ public class GoogleStepDefinition {
         Assert.assertEquals(expectedTitle, actualTitle);
 
     }
-
 
 
 }
